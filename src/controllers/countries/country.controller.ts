37925029629826da2d970c9783dc6ac74d75510f
Zhayvoronok Kateryna <1gayv425@gmail.com>
2025-02-05 16:11:12 +0200

    added /country/:countryCode endpoint

--- src/controllers/countries/country.controller.ts
+++ src/controllers/countries/country.controller.ts
@@ -1,13 +1,20 @@
 import { CountryService } from 'src/services/countries/country.service';
-import { Controller, Get } from '@nestjs/common';
-import { Country } from 'src/types/country.types';
+import { Controller, Get, Param } from '@nestjs/common';
+import { Country, CountryDetails } from 'src/types/country.types';
 
-@Controller('')
+@Controller('countries')
 export class CountriesController {
   constructor(private readonly countryService: CountryService) {}
 
-  @Get('countries')
+  @Get()
   async getAvailableCountries(): Promise<Country[]> {
     return await this.countryService.getAvailableCountries();
+  }
+
+  @Get(':countryCode')
+  async getCountryDetails(
+    @Param('countryCode') countryCode: string,
+  ): Promise<CountryDetails> {
+    return this.countryService.getCountryDetails(countryCode);
   }
 }
--- src/services/countries/country.service.ts
+++ src/services/countries/country.service.ts
@@ -1,33 +1,80 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { Country } from 'src/types/country.types';
+import type {
+  Country,
+  CountryDetails,
+  CountryInfo,
+  FlagData,
+  PopulationData,
+} from 'src/types/country.types';
 
 @Injectable()
 export class CountryService {
   private readonly availableCountryApi: string;
+  private readonly countryInfoApi: string;
+  private readonly populationApi: string;
+  private readonly flagApi: string;
 
   constructor(private readonly configService: ConfigService) {
     this.availableCountryApi = this.configService.get<string>(
       'API_AVAILABLE_COUNTRIES',
       '',
     );
+    this.countryInfoApi = this.configService.get<string>(
+      'API_COUNTRY_INFO',
+      '',
+    );
+    this.populationApi = this.configService.get<string>('API_POPULATION', '');
+    this.flagApi = this.configService.get<string>('API_FLAG', '');
   }
 
   async getAvailableCountries(): Promise<Country[]> {
-    try {
-      const response = await fetch(this.availableCountryApi);
-      if (!response.ok) {
-        throw new Error(`HTTP Error: ${response.status}`);
-      }
-      const data = (await response.json()) as Country[];
-      return data;
-    } catch (error) {
+    const response = await fetch(this.availableCountryApi);
+    if (!response.ok) {
       throw new HttpException(
-        `Failed to fetch available countries: ${
-          error instanceof Error ? error.message : 'Unknown error'
-        }`,
+        `Fetch failed: ${response.statusText}`,
         HttpStatus.BAD_REQUEST,
       );
     }
+    const data = (await response.json()) as Country[];
+
+    return data;
+  }
+
+  async getCountryDetails(countryCode: string): Promise<CountryDetails> {
+    const countries = await this.getAvailableCountries();
+    const country = countries.find((c) => c.countryCode === countryCode);
+    if (!country)
+      throw new HttpException(
+        `Country ${countryCode} not found`,
+        HttpStatus.NOT_FOUND,
+      );
+    const countryInfoRes = await fetch(`${this.countryInfoApi}/${countryCode}`);
+    const countryInfo = (await countryInfoRes.json()) as CountryInfo;
+
+    const populationRes = await fetch(this.populationApi, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ country: country.name }),
+    });
+
+    const populationData = (await populationRes.json()) as PopulationData;
+
+    const flagRes = await fetch(this.flagApi, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ country: country.name }),
+    });
+    const flagData = (await flagRes.json()) as FlagData;
+
+    return {
+      country: {
+        name: country.name,
+        countryCode: country.countryCode,
+      },
+      countryInfo: countryInfo.borders,
+      populationData: populationData.data.populationCounts,
+      flagData: flagData.data.flag,
+    };
   }
 }
