a457e0810c08fd0cb378b6b7492f92aed72fdb78
Zhayvoronok Kateryna <1gayv425@gmail.com>
2025-02-05 15:01:03 +0200

    added .env.dev file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CountriesModule } from './modules/countries/country.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CountriesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env.dev',
+    }),
+    CountriesModule,
+  ],
 })
 export class AppModule {}
--- src/services/countries/country.service.ts
+++ src/services/countries/country.service.ts
@@ -1,12 +1,21 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Country } from 'src/types/country.types';
 
 @Injectable()
 export class CountryService {
-  private readonly apiUrl = 'https://date.nager.at/api/v3/AvailableCountries';
+  private readonly availableCountryApi: string;
+
+  constructor(private readonly configService: ConfigService) {
+    this.availableCountryApi = this.configService.get<string>(
+      'API_AVAILABLE_COUNTRIES',
+      '',
+    );
+  }
+
   async getAvailableCountries(): Promise<Country[]> {
     try {
-      const response = await fetch(this.apiUrl);
+      const response = await fetch(this.availableCountryApi);
       if (!response.ok) {
         throw new Error(`HTTP Error: ${response.status}`);
       }
